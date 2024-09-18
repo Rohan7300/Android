4b1ff817c563332600c4827066d04ed393f12283
Rohan7300 <105407947+Rohan7300@users.noreply.github.com>
2024-09-18 18:19:45 +0530

    Update MainActivity.kt

--- app/src/main/java/com/notify/testproject/MainActivity.kt
+++ app/src/main/java/com/notify/testproject/MainActivity.kt
@@ -14,19 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w("FCM", "Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
-            }
 
-            // Get new FCM registration token
-            val token = task.result
-            Log.d("FCM", token)
-            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
-
-            val serviceIntent = Intent(this, ForegroundService::class.java)
-            ContextCompat.startForegroundService(this, serviceIntent)
-        })
     }
 }
